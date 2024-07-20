3affa8a296d6ad49d91266ae1b1e44a3c51bcab9
YQisme <1398757912@qq.com>
2024-07-21 02:05:28 +0800

    查找bug

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: "/about",
   assetPrefix: "/about/",
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
